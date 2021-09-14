e515dfd421f6bd1b1eeacc5eb9cec4eeb474fb76
ostapetsk <ostapkur@gmail.com>
2021-09-14 23:13:15 +0300

    second commit

--- project1/Program.cs
+++ project1/Program.cs
@@ -7,6 +7,7 @@ namespace project1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is my second commit");
         }
     }
 }
